a03afea464c89813dd8e4b6db9a6bd7c6dda285d
5samagi <96784101+5samagi@users.noreply.github.com>
2021-12-29 14:08:33 +0530

    Update help.py

--- WebStreamer/bot/plugins/help.py
+++ WebStreamer/bot/plugins/help.py
@@ -1,3 +1,5 @@
+from WebStreamer.bot import StreamBot
+
 @StreamBot.on_message(filters.command('help'))
 async def help(_, m: Message):
     await m.reply("Didn not work? Give a clickdidn't work give a click https://gotalink.herokuapp.com/")
