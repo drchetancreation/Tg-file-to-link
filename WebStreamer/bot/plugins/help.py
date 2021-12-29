50f998a49b404ba3621006c95906179778b45e16
5samagi <96784101+5samagi@users.noreply.github.com>
2021-12-29 13:30:01 +0530

    Update help.py

--- WebStreamer/bot/plugins/help.py
+++ WebStreamer/bot/plugins/help.py
@@ -1,2 +1,2 @@
-def help(update: Update, context: CallbackContext):
-    update.message.reply_text("Didn't work? Give a clickdidn't work give a click https://gotalink.herokuapp.com/")
+def helpon(update: Update, context: CallbackContext):
+    update.message.reply_text("Didn not work? Give a clickdidn't work give a click https://gotalink.herokuapp.com/")
