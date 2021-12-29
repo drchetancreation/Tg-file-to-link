9fc94683f353bc61ff2526607ed6fc202723d9f6
5samagi <96784101+5samagi@users.noreply.github.com>
2021-12-29 14:58:05 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,7 +6,7 @@ from WebStreamer.vars import Var
 from pyrogram import filters, emoji
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.command(['start', 'help']))
+@StreamBot.on_message(filters.command('start'))
 async def start(b, m):
     await m.reply('Hi, Send me a file to get an instant stream link.',
                   reply_markup=InlineKeyboardMarkup(
