d6ff17f17bf64ceb37cceeb600c4c1396c91cc52
5samagi <96784101+5samagi@users.noreply.github.com>
2022-01-01 21:32:03 +0530

    Update help.py

--- WebStreamer/bot/plugins/help.py
+++ WebStreamer/bot/plugins/help.py
@@ -2,6 +2,12 @@ from pyrogram import filters
 from WebStreamer.bot import StreamBot
 from pyrogram.types import Message
 
+
+
 @StreamBot.on_message(filters.command('help'))
 async def help(_, m: Message):
-    await m.reply("Didn not work? Give a click https://gotalink.herokuapp.com/")
+    await m.reply("If didn't work, look bot info^ not/")
+
+@StreamBot.on_message(filters.command('info'))
+async def info(_, m: Message):
+    await m.reply("Video, Audio, Document >» stream, download links")  
