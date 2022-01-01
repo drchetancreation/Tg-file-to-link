bdfce0eee54d949f583057681e5a83252f383161
5samagi <96784101+5samagi@users.noreply.github.com>
2022-01-01 21:39:17 +0530

    Update help.py

--- WebStreamer/bot/plugins/help.py
+++ WebStreamer/bot/plugins/help.py
@@ -2,12 +2,10 @@ from pyrogram import filters
 from WebStreamer.bot import StreamBot
 from pyrogram.types import Message
 
-
-
 @StreamBot.on_message(filters.command('help'))
 async def help(_, m: Message):
     await m.reply("If didn't work, look bot info^ not/")
 
 @StreamBot.on_message(filters.command('info'))
 async def info(_, m: Message):
-    await m.reply("Video, Audio, Document >» stream, download links")  
+    await m.reply("Video, Audio, Document >» stream, download links")
